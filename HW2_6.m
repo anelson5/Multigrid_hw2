207eff987955adb8d86cd71c2853e927f8b355a0
anelson5 <anelson@math.utah.edu>
2016-03-14 11:53:57 -0400

    Added files via upload
    final v cycle with images used for homework

--- HW2_6.m
+++ HW2_6.m
@@ -16,29 +16,71 @@
 
 clear
 clc
+
+errorvect = zeros(1,1); 
 nu1 = 2; 
-nu2 = 2; 
+nu2 = 1; 
 w = 2/3; 
-n=32; 
+n=64; 
+n1 = n; 
 h=1/n; 
 x = 0:h:1;
 %Need to have vh be length n-1
 
-levels = 2; 
 
-x1 = x(2:end-1); 
-v =zeros(size(x1)); 
-f = ones(length(x)-2,1);
-[v] = vcycle(h,f,v,levels, nu1, nu2);
+%homogeneous f(x)
+b = 0;
+%inhomogeneous f(x) = 1
+%b = 1; 
+switch b
+    case 0 
+        x1 = x(2:end-1); 
+        v = sin(2*pi*9*x1)
+        %v = 20*ones(size(x1));
+        f = zeros(length(x)-2,1);
+        true = zeros(size(x)); 
+        figure(b+1)
+    case 1
+        x1 = x(2:end-1); 
+        v = sin(2*pi*9*x1); 
+        v = ones(size(x1));
+        f = ones(length(x)-2,1);
+        true = -0.5*x.^2 + 0.5*x;
+        figure(b)
+end
 
-%Gives the solution at the coarsest grid
-n=32; 
-h=1/n; 
-x = 0:h:1;
-true = -0.5*x.^2 + 0.5*x; 
-plot(x,true); 
+%Initialize guess for v and rhs
+
+v0 = [0 v 0]; 
+error1 = max(abs((v0 - true))); 
+threshold = error1/10^5
+%threshold = 1e-5; 
+counter = 1; 
+plot(x,v0,'Linewidth',2); 
 hold on; 
-this = v; 
-this = this'
-y = [0 this 0]; 
-plot(x,y,'rx')
+while error1 > threshold
+    errorvect(counter) = error1;
+    [v] = vcycle(h,f,v, nu1, nu2);
+%Gives the solution at the coarsest grid
+h=1/n1; 
+x = 0:h:1;
+%true = zeros(size(x)); 
+plot(x,true); 
+hold on;
+v = v'; 
+y = [0 v 0]; 
+plot(x,y,'rx','Linewidth',2)
+error1 = max(abs(true-y));
+counter = counter + 1;
+end
+error1
+ set(gca,'fontsize',20)
+figure(2)
+semilogy(errorvect,'r','Linewidth',2)
+ title('Log plot of error vs number of V-cycles (n=64)','fontsize',20)
+  set(gca,'fontsize',18)
+figure(3)
+plot(x,y,'rx-', x, true,'Linewidth',2); 
+title('Plot of last V-cycle result $d_{xx} = \delta $(n=64)','fontsize',20,'Interpreter','latex'); 
+hold on; 
+ set(gca,'fontsize',20)
